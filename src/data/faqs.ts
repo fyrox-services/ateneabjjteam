7197af71ed0182aed86c67d6a25c187f42213a98
Miguel Armas <miguelalbertoaz@gmail.com>
2025-07-14 17:16:57 +0200

    corrige estilos del componente faqs

--- src/data/faqs.ts
+++ src/data/faqs.ts
@@ -5,4 +5,23 @@ export const FAQS = [
       "Sí, puedes probar de forma totalmente gratuita la clase de kimono y sin kimono.",
     ],
   },
+  {
+    heading: "¿Cómo debo ir vestido el primer día?",
+    text: [
+      "Si la disciplina que vas a probar es NoGi, deberás traer una camiseta y un pantalón corto deportivo que no tenga cremalleras ni nada que pueda hacer daño al roce.",
+      "Si por el contrario, la disciplina que vas a probar es Kimono, deberás traer una camiseta para ponerte debajo del Gi. En caso de no tener tu propio kimono ponte en contacto con nosotros antes de tu clase de prueba y sabiendo tus medidas te prestaremos uno.",
+    ],
+  },
+  {
+    heading: "Nunca antes he practicado jiu jitsu. ¿Es necesario tener experiencia para poder entrenar con vosotros?",
+    text: [
+      "No es necesario tener experiencia previa, todos hemos tenido un primer día. En Atenea todas las clases están adaptadas para todos los niveles y tendrás una persona avanzada acompañándote para orientarte y ayudarte en lo que necesites."
+    ],
+  },
+  {
+    heading: "¿Necesito estar en buena forma física para practicar jiu jitsu?",
+    text: [
+      "Evidentemente, si eres una persona deportista y con buena forma física, influirá en tu rendimiento en el tatami. Sin embargo, no es necesario, ya que el propio jiu jitsu poco a poco irá mejorando tus capacidades físicas. Además, contamos con un programa de preparación física adaptado al bjj con un instructor que imparte clases personalizadas que te ayudará aún más a conseguir tus objetivos."
+    ],
+  },
 ];
