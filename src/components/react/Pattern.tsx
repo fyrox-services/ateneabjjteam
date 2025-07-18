d7bc8ed9c11c1c944b55ecd9883142ea304144d2
mikecode <miguelalbertoaz@gmail.com>
2025-07-18 16:41:11 +0200

    agrega diseño para las página principales

--- src/components/react/Pattern.tsx
+++ src/components/react/Pattern.tsx
@@ -1,16 +1,44 @@
-export function Pattern({ children }: { children: any }) {
-  return (
-    <div className="min-h-screen w-full bg-white relative">
-      {/* Diagonal Stripes Background */}
-      <div
-        className="absolute inset-0 z-0"
-        style={{
-          backgroundImage:
-            "repeating-linear-gradient(45deg, transparent, transparent 2px, #f3f4f6 2px, #f3f4f6 4px)",
-        }}
-      />
-      {children}
-      {/* Your Content/Components */}
-    </div>
-  );
+export function Pattern({
+  children,
+  type = 1,
+}: {
+  children: any;
+  type?: number;
+}) {
+  if (type === 1) {
+    return (
+      <div className="min-h-screen w-full bg-white relative">
+        {/* Diagonal Stripes Background */}
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage:
+              "repeating-linear-gradient(45deg, transparent, transparent 2px, #f3f4f6 2px, #f3f4f6 4px)",
+          }}
+        />
+        {children}
+        {/* Your Content/Components */}
+      </div>
+    );
+  }
+
+  if (type === 2) {
+    return (
+      <div className="w-full bg-white relative">
+        {/* Amber Glow Background */}
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            backgroundImage: `
+        radial-gradient(125% 125% at 50% 90%, #ffffff 40%, var(--secondary) 100%)
+      `,
+            backgroundSize: "100% 100%",
+          }}
+        />
+        {children}
+
+        {/* Your Content/Components */}
+      </div>
+    );
+  }
 }
--- src/helpers/handleMenuMobile.ts
+++ src/helpers/handleMenuMobile.ts
@@ -1,7 +1,9 @@
 const btnMenuMobile = document.getElementById("btn-menu-mobile");
 const headerNav = document.getElementById("header-nav");
+const businessName = document.getElementById("business-name");
 
 btnMenuMobile?.addEventListener("click", () => {
   btnMenuMobile.classList.toggle("active");
   headerNav?.classList.toggle("active");
+  businessName?.classList.toggle("active");
 });
