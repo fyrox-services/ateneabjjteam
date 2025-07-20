8f2a225fd18a57a7a123cec5438d8389fc420ec7
mikecode <miguelalbertoaz@gmail.com>
2025-07-20 15:02:30 +0200

    agrega diseño de precios

---
+++ src/helpers/formatCurrency.ts
@@ -0,0 +1,7 @@
+export function formatearMoneda(value: number) {
+  return new Intl.NumberFormat("es-ES", {
+    style: "currency",
+    currency: "EUR",
+    minimumFractionDigits: 0,
+  }).format(value);
+}
