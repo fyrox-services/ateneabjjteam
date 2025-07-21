f03ca39688881a066ddbab1279e42f798ba569f6
mikecode <miguelalbertoaz@gmail.com>
2025-07-21 10:54:22 +0200

    agrega imágenes a la página de escuela

---
+++ src/data/images.ts
@@ -0,0 +1,31 @@
+import black_belt from "@/assets/school/black_belt.png";
+import faqs from "@/assets/school/faqs.jpg";
+import girls from "@/assets/school/girls.jpg";
+import graduation from "@/assets/school/graduation.jpeg";
+import tatami from "@/assets/school/tatami.jpg";
+import rotulo from "@/assets/school/rotulo.jpeg";
+
+import abudhabi from "@/assets/space/abudhabi.jpg";
+import escritorio from "@/assets/space/escritorio.jpg";
+import explain from "@/assets/space/explain.jpg";
+import kids from "@/assets/space/kids.jpeg";
+import medallas from "@/assets/space/medallas.jpg";
+import ola from "@/assets/space/ola.jpg";
+import ropa from "@/assets/space/ropa.jpg";
+import trofeos from "@/assets/space/trofeos.jpg";
+
+export const imagesHome = [graduation, rotulo, girls, black_belt, faqs, 
+abudhabi
+
+];
+
+export const imagesSchool = [
+  tatami,
+  escritorio,
+  explain,
+  kids,
+  medallas,
+  ola,
+  ropa,
+  trofeos,
+];
