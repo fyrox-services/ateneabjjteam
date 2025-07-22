505d11ac34c58794d97771d78892708254a50772
mikecode <miguelalbertoaz@gmail.com>
2025-07-22 10:06:42 +0200

    agrega btn float en desktop

--- src/helpers/handleBtnFloat.ts
+++ src/helpers/handleBtnFloat.ts
@@ -1,6 +1,10 @@
+import { isModalActive } from "@/stores";
+
 const btnFloatWrap = document.getElementById("btn-float-wrap");
 const btnFloatMobile = document.getElementById("btn-float-mobile");
 const hiddenBtns = document.getElementById("hidden-btns");
+const btnFreeClass = document.getElementById("btn-free-class");
+const btnFreeClassDesktop = document.getElementById("btn-free-class-desktop");
 
 btnFloatMobile?.addEventListener("click", (event) => {
   event.stopPropagation();
@@ -11,10 +15,18 @@ document.body.addEventListener("click", () => {
   hiddenBtns?.classList.remove("active");
 });
 
-window.addEventListener("scroll", (event) => {
+window.addEventListener("scroll", () => {
   if (window.scrollY >= window.innerHeight) {
     btnFloatWrap?.classList.add("active");
   } else {
     btnFloatWrap?.classList.remove("active");
   }
+});
+
+btnFreeClass?.addEventListener("click", () => {
+  isModalActive.set(true);
+});
+
+btnFreeClassDesktop?.addEventListener("click", () => {
+  isModalActive.set(true);
 });
