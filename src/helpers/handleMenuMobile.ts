d74a89e1bbf03eacbe26aebbba46f676b9c47a64
mikecode <miguelalbertoaz@gmail.com>
2026-02-09 21:14:02 +0100

    agrega cerrar navbar con cualquier enlace

--- src/helpers/handleMenuMobile.ts
+++ src/helpers/handleMenuMobile.ts
@@ -2,9 +2,20 @@ const btnMenuMobile = document.getElementById("btn-menu-mobile");
 const headerNav = document.getElementById("header-nav");
 const businessName = document.getElementById("business-name");
 
+function closeMenu() {
+  btnMenuMobile?.classList.remove("active");
+  headerNav?.classList.remove("active");
+  businessName?.classList.remove("active");
+  document.body.classList.remove("not-scrollable");
+}
+
 btnMenuMobile?.addEventListener("click", () => {
   btnMenuMobile.classList.toggle("active");
   headerNav?.classList.toggle("active");
   businessName?.classList.toggle("active");
   document.body.classList.toggle("not-scrollable");
+});
+
+headerNav?.querySelectorAll("a").forEach((link) => {
+  link.addEventListener("click", closeMenu);
 });
